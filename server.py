41590ac856effb8771520125d50c661717a6edf9
Andrei Neagoie <aneagoie1@gmail.com>
2019-10-24 05:41:37 -0400

    Update server.py

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ def write_to_file(data):
     file = database.write(f'\n{email},{subject},{message}')
 
 def write_to_csv(data):
-  with open('database.csv', mode='a') as database2:
+  with open('database.csv', newline='', mode='a') as database2:
     email = data["email"]
     subject = data["subject"]
     message = data["message"]
